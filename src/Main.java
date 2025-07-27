72ddcb1fb501df947fea4e52cc12dc9af4a0a049
merve-goncu <merve.gnc.946@gmail.com>
2025-07-27 15:46:29 +0300

    Differences of map types

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,36 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Map<Integer, String> hashMap = new HashMap<>();
+        Map<Integer, String> linkedHashMap = new LinkedHashMap<>();
+        Map<Integer, String> treeMap = new TreeMap<>();
 
-        Map<Integer, String> studentNumber = new HashMap<>();
+        // Aynı veriler ekleniyor
+        hashMap.put(3, "Üç");
+        hashMap.put(1, "Bir");
+        hashMap.put(2, "İki");
 
-        studentNumber.put(7454,"Merve");
-        studentNumber.put(7050,"Binali");
-        studentNumber.put(1453,"Mehmet");
+        linkedHashMap.put(3, "Üç");
+        linkedHashMap.put(1, "Bir");
+        linkedHashMap.put(2, "İki");
 
-        for(Integer  number : studentNumber.keySet()){
-            System.out.println(number);
+        treeMap.put(3, "Üç");
+        treeMap.put(1, "Bir");
+        treeMap.put(2, "İki");
+
+        System.out.println("HashMap:");
+        for (Map.Entry<Integer, String> entry : hashMap.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
         }
 
-        for(String name : studentNumber.values()){
-            System.out.println(name);
+        System.out.println("\nLinkedHashMap:");
+        for (Map.Entry<Integer, String> entry : linkedHashMap.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
         }
 
-        for (Map.Entry<Integer, String> entry : studentNumber.entrySet()){
-            System.out.println(entry.getKey()+":"+ entry.getValue());
+        System.out.println("\nTreeMap:");
+        for (Map.Entry<Integer, String> entry : treeMap.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
         }
     }
 }
