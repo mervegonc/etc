640ca00d4f426ab1aaa15494cdf207a40c19bc10
merve-goncu <merve.gnc.946@gmail.com>
2025-07-27 13:54:58 +0300

    abstract+interface

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+TeslaCar car = new TeslaCar();
+car.fuel ="electric";
+car.charge();
+car.move();
+
+
+    }
+
+
+}
+
+
+
+
---
+++ src/Vehicle.java
@@ -0,0 +1,4 @@
+public abstract class Vehicle {
+String fuel; //field
+    public abstract void move();
+}
