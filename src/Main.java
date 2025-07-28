cc59e5987596276eb039bc40bc18b62272116618
merve-goncu <merve.gnc.946@gmail.com>
2025-07-28 09:38:04 +0300

    Useage of filter future

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> list = List.of("Elma", "Armut", "Kiraz");
+        List<String> list = List.of("Ali", "Ayşe", "Veli");
 
-        list.stream().forEach(item -> System.out.println("Meyve: " + item));
+        list.stream()
+                .filter(name -> name.startsWith("A"))
+                .forEach(name -> System.out.println("A ile başlayan: " + name));
     }
 }
