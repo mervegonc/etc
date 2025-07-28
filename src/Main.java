d8bb0c01ceba5cd4c8868012b8ed386d232cc4f0
merve-goncu <merve.gnc.946@gmail.com>
2025-07-28 09:35:21 +0300

    Stream API (Java 8+ future)/basic example of forEach

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("Ali", "Ayşe", "Mehmet");
+        List<String> list = List.of("Elma", "Armut", "Kiraz");
 
-        names.stream()
-                .filter(name -> name.startsWith("A"))
-                .forEach(name -> System.out.println("A ile başlayan: " + name));
+        list.stream().forEach(item -> System.out.println("Meyve: " + item));
     }
 }
